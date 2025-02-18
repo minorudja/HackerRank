83abe48b6131a3eabfbc678dca3d1d184480c6ae
minorudananjaya <115642409+minorudananjaya@users.noreply.github.com>
2025-02-18 12:33:44 +0000

    Update

--- Artificial Intelligence/Morphological_Operations_Closing/Morphological_Operations_Closing.py
+++ Artificial Intelligence/Morphological_Operations_Closing/Morphological_Operations_Closing.py
@@ -4,6 +4,24 @@ def checkIfWithinImage(x, y, image):
     else:
         return False
 
+def morphDilation(elemS, elemSOrigin, image, rows, cols):
+    dilatedImg = [[0 for i in range(cols)] for j in range(rows)]
+    elemSRelCoords = []
+    for r in range(len(elemS)):
+        for c in range(len(elemS[0])):
+            if (r,c) != elemSOrigin:
+                elemSRelCoords.append([r-elemSOrigin[0],c-elemSOrigin[1]])
+
+    for i in range(rows):
+        for j in range(cols):
+            if image[i][j] == 1:
+                dilatedImg[i][j] = elemS[elemSOrigin[0]][elemSOrigin[1]]
+                for coord in elemSRelCoords:
+                    if checkIfWithinImage(i+coord[0],j+coord[1],dilatedImg):
+                        dilatedImg[i+coord[0]][j+coord[1]] = elemS[elemSOrigin[0]+coord[0]][elemSOrigin[1]+coord[1]]
+
+    return dilatedImg
+
 def morphErosion(elemS, elemSOrigin, image, rows, cols):
     erosionImg = [[0 for i in range(cols)] for j in range(rows)]
     elemSRelCoords = []
@@ -33,6 +51,11 @@ def morphErosion(elemS, elemSOrigin, image, rows, cols):
 
     return erosionImg
 
+def morphOpening(elemS, elemSOrigin, image, rows, cols):
+    dilationImg = morphDilation(elemS, elemSOrigin, image, rows, cols)
+    openImg = morphErosion(elemS, elemSOrigin, dilationImg, rows, cols)
+
+    return openImg
 
 ## Main Program
 image = [[0,0,0,0,0,0,0,0,0,0],
@@ -52,11 +75,11 @@ elemS = [[1,1,1],
 elemSOrigin = (1,1)
 rows, cols = (len(image),len(image[0]))
 
-erosionImg = morphErosion(elemS, elemSOrigin, image, rows, cols)
+openImg = morphOpening(elemS, elemSOrigin, image, rows, cols)
 
 noPixelOnes = 0
 for ind in range(rows):
-    # print(erosionImg[ind])
-    noPixelOnes += erosionImg[ind].count(1)
+    # print(openImg[ind])
+    noPixelOnes += openImg[ind].count(1)
 
 print(noPixelOnes)
